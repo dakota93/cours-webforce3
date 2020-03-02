6fcc36336145fe6c0d128c6353e83f243d7714ad
ld <christian.beneteau@lepoles.com>
2020-03-02 16:55:25 +0100

    gg

---
+++ cours_php.php
@@ -0,0 +1,60 @@
+jour 1 à 205: les bases
+jours 2.5 à 4: les super globales
+jour 5 à 6 : pdo
+jours 7  à 10: site e-commerce
+
+-------------------------------------------------------------------
+
+jour 1/10
+- introduction 
+- verifier XAMPP/MAMP
+
+- les bases :
+    - balise PHP , affichage 
+    - variable concatenation, guillemet et quotes
+    - constante, opérateurs arithmétiqus
+    - strutures conditionelles
+    - switch
+
+-------------------------------------------------------------------------
+
+PHP
+
+hypertext preProcessor
+
+    - la,gage de programmation de SCRIPT
+    - interpretation coté SERVEUR
+    - spécifiquement créer pour faire des sites web
+
+
+--------------------------------------------------------------------------------
+
+PHP: 
+     1- on récupére les données 
+     2- on vérifie les données$
+     3- on interoge la BDD
+     4- on envoie la réponse au CLIENT (navigateur)
+
+
+site dynamique:
+                1-base de donnée 
+                2- un front
+                3- back office
+
+
+architecture CLIENT-SERVEUR
+
+CLIENT > REQUETE HTTP > SERVEUR > (étape intermediaire interpreteur php) > VOTRE SITE.COM
+
+-------------------------------------------------------------------------------------------
+
+X : Cross
+A : Apache
+M : MySQL
+P : PHO
+P : Perl
+
+----------------------------------------------------------------------------------------------
+
+
+
